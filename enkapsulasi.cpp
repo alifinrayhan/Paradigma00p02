76a66516f30e9d6f1b1273928900bbcd69f228c9
alifinrayhan@gmail.com <alifinrayhan@gmail.com>
2024-05-27 15:33:00 +0700

    int main

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -12,3 +12,19 @@ public:
         return sakLarNo[i];
     }
 };
+
+int main() {
+    remotelampu LampuRumah;
+
+    LampuRumah.setSaklarNo(0, "lampu Tras Rumah");
+    LampuRumah.setSaklarNo(1, "Lampu Ruang Tamu");
+    LampuRumah.setSaklarNo(2, "lampu Kamar Tidue");
+    LampuRumah.setSaklarNo(3, "Lampu dapur");
+
+    cout << LampuRumah.getSaklarNo(0) << endl;
+    cout << LampuRumah.getSaklarNo(1) << endl;
+    cout << LampuRumah.getSaklarNo(2) << endl;
+    cout << LampuRumah.getSaklarNo(3) << endl;
+
+    return 0;
+}
