45ad0d8b8fc38fe6aa0ed2d8559dd2734bce746c
alifinrayhan@gmail.com <alifinrayhan@gmail.com>
2024-05-27 15:07:09 +0700

    class pelajar

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -21,3 +21,12 @@ public:
         cout << "Pekerja dibuat\n" << endl;
     }    
 };
+
+class pelajar : virtual public orang {
+public:
+    pelajar(int pUmur) :
+        orang(pUmur)
+    {
+        cout << "Pelajar di buat\n" << endl;
+    }    
+};
